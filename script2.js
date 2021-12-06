90fa4880963f09d668f1ce0a6dc639b0ad0580c2
Angela Ciobanu <92688061+Dydee27@users.noreply.github.com>
2021-12-07 00:16:02 +0100

    local storage

---
+++ script2.js
@@ -0,0 +1,10 @@
+const Local_Storage_Key = "task.storage";
+let storage = JSON.parse(localStorage.getItem(Local_Storage_Key)) || [];
+
+function render() {
+	save();
+	render();
+}
+function save() {
+	localStorage.setItem(Local_Storage_Key, JASON.stringify(storage));
+}
